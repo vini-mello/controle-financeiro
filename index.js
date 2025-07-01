3049d91c12f764e9bae346db98b7ac396c35b1cf
vini-mello <vbatera@hotmail.com>
2025-07-01 11:12:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 document.getElementById('configForm').addEventListener('submit', function(e) {
     e.preventDefault();
+    const mes = document.getElementById('mes').value;
     const dataInicio = document.getElementById('dataInicio').value;
     const dataFim = document.getElementById('dataFim').value;
     const ticket = document.getElementById('ticket').value;
 
-    if (dataInicio && dataFim && ticket) {
+    if (mes && dataInicio && dataFim && ticket) {
+        localStorage.setItem('mes', mes);
         localStorage.setItem('dataInicio', dataInicio);
         localStorage.setItem('dataFim', dataFim);
         localStorage.setItem('ticket', ticket);
-        localStorage.setItem('mes', new Date(dataInicio).getMonth() + 1); // Mes atual
         alert('Configurações salvas! Redirecionando para lançamentos...');
         window.location.href = 'lancamentos.html';
     } else {
         alert('Preencha todos os campos!');
     }
 });
+
+function toggleDarkMode() {
+    const body = document.body;
+    const toggle = document.getElementById("darkModeToggle");
+    body.classList.toggle("dark-mode");
+    const isDarkMode = body.classList.contains("dark-mode");
+    localStorage.setItem("darkMode", isDarkMode);
+    toggle.checked = isDarkMode;
+}
